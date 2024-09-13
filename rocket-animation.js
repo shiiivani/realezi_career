ef806675a6259d34336576508dbb6810b5661b1f
Your Name <you@example.com>
2024-09-13 13:44:32 +0530

    Initial Commit

---
+++ rocket-animation.js
@@ -0,0 +1,137 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let mm = gsap.matchMedia();
+
+mm.add("(min-width: 1200px) and (max-height: 900px)", function () {
+  const rocketTimelineLarge = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".navbar", // Trigger animation based on the navbar section
+      start: "top top", // Animation starts when the top of .navbar hits the top of the viewport
+      endTrigger: ".join-us-section", // Animation ends when .join-us-section hits the top of the viewport
+      end: "top top", // End animation when the top of .join-us-section hits the top of the viewport
+      scrub: true, // Smoothly animate the rocket
+      markers: false,
+    },
+  });
+
+  rocketTimelineLarge
+    .to(".rocket", { duration: 0.05, x: -150, y: 100, rotation: 35 })
+    .to(".rocket", { duration: 0.05, y: 300, rotation: -60 })
+    .to(".rocket", { duration: 0.05, x: 600, y: 500, rotation: -60 })
+    .to(".rocket", { duration: 0.05, y: 600, rotation: 60 })
+    .to(".rocket", { duration: 0.05, x: -200, y: 950, rotation: 60 })
+    .to(".rocket", { duration: 0.05, y: 1050, rotation: -55 })
+    .to(".rocket", { duration: 0.05, x: 260, y: 1200, rotation: -55 })
+    .to(".rocket", { duration: 0.05, x: 385, y: 1300, rotation: 0 })
+    .to(".rocket", { duration: 0.05, y: 1480, rotation: 0 });
+});
+
+mm.add("(min-width: 1200px) and (min-height: 900px)", function () {
+  const rocketTimelineLarge = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".navbar", // Trigger animation based on the navbar section
+      start: "top top", // Animation starts when the top of .navbar hits the top of the viewport
+      endTrigger: ".join-us-section", // Animation ends when .join-us-section hits the top of the viewport
+      end: "top top", // End animation when the top of .join-us-section hits the top of the viewport
+      scrub: true, // Smoothly animate the rocket
+      markers: false,
+    },
+  });
+
+  rocketTimelineLarge
+    .to(".rocket", { duration: 0.05, x: -150, y: 100, rotation: 35 })
+    .to(".rocket", { duration: 0.05, y: 250, rotation: -60 })
+    .to(".rocket", { duration: 0.05, x: 600, y: 450, rotation: -60 })
+    .to(".rocket", { duration: 0.05, y: 600, rotation: 60 })
+    .to(".rocket", { duration: 0.05, x: -200, y: 750, rotation: 60 })
+    .to(".rocket", { duration: 0.05, y: 1000, rotation: -55 })
+    .to(".rocket", { duration: 0.05, x: 260, y: 1300, rotation: -55 })
+    .to(".rocket", { duration: 0.09, x: 385, y: 1400, rotation: 0 })
+    .to(".rocket", { duration: 0.09, y: 1500, rotation: 0 });
+});
+
+mm.add("(min-width: 1200px) and (min-height: 1000px)", function () {
+  const rocketTimelineLarge = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".navbar", // Trigger animation based on the navbar section
+      start: "top top", // Animation starts when the top of .navbar hits the top of the viewport
+      endTrigger: ".join-us-section", // Animation ends when .join-us-section hits the top of the viewport
+      end: "top top", // End animation when the top of .join-us-section hits the top of the viewport
+      scrub: true, // Smoothly animate the rocket
+      markers: false,
+    },
+  });
+
+  rocketTimelineLarge
+    .to(".rocket", { duration: 0.05, x: -150, y: 100, rotation: 35 })
+    .to(".rocket", { duration: 0.05, y: 250, rotation: -60 })
+    .to(".rocket", { duration: 0.05, x: 600, y: 450, rotation: -60 })
+    .to(".rocket", { duration: 0.05, y: 600, rotation: 60 })
+    .to(".rocket", { duration: 0.05, x: -200, y: 750, rotation: 60 })
+    .to(".rocket", { duration: 0.05, y: 1000, rotation: -55 })
+    .to(".rocket", { duration: 0.05, x: 260, y: 1300, rotation: -55 })
+    .to(".rocket", { duration: 0.09, x: 385, y: 1400, rotation: 0 })
+    .to(".rocket", { duration: 0.09, y: 1550, rotation: 0 });
+});
+
+mm.add("(min-width: 1100px) and (max-width: 1200px)", function () {
+  const rocketTimelineLarge = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".navbar", // Trigger animation based on the navbar section
+      start: "top top", // Animation starts when the top of .navbar hits the top of the viewport
+      endTrigger: ".join-us-section", // Animation ends when .join-us-section hits the top of the viewport
+      end: "top top", // End animation when the top of .join-us-section hits the top of the viewport
+      scrub: true, // Smoothly animate the rocket
+      markers: false,
+    },
+  });
+
+  rocketTimelineLarge
+    .to(".rocket", { duration: 0.05, x: -150, y: 150, rotation: 35 })
+    .to(".rocket", { duration: 0.05, y: 250, rotation: -60 })
+    .to(".rocket", { duration: 0.05, x: 650, y: 400, rotation: -60 })
+    .to(".rocket", { duration: 0.05, y: 600, rotation: 60 })
+    .to(".rocket", { duration: 0.05, x: -100, y: 750, rotation: 60 })
+    .to(".rocket", { duration: 0.05, y: 950, rotation: -55 })
+    .to(".rocket", { duration: 0.05, x: 260, y: 1100, rotation: -55 })
+    .to(".rocket", { duration: 0.2, x: 375, y: 1300, rotation: 0 })
+    .to(".rocket", { duration: 0.2, y: 1600, rotation: 0 });
+});
+
+mm.add("(min-width: 990px) and (max-width: 1100px)", function () {
+  const rocketTimelineLarge = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".navbar", // Trigger animation based on the navbar section
+      start: "top top", // Animation starts when the top of .navbar hits the top of the viewport
+      endTrigger: ".join-us-section", // Animation ends when .join-us-section hits the top of the viewport
+      end: "top top", // End animation when the top of .join-us-section hits the top of the viewport
+      scrub: true, // Smoothly animate the rocket
+      markers: false,
+    },
+  });
+
+  rocketTimelineLarge
+    .to(".rocket", { duration: 0.05, x: -90, y: 100, rotation: 35 })
+    .to(".rocket", { duration: 0.05, y: 250, rotation: -60 })
+    .to(".rocket", { duration: 0.05, x: 650, y: 400, rotation: -60 })
+    .to(".rocket", { duration: 0.05, y: 600, rotation: 60 })
+    .to(".rocket", { duration: 0.05, x: -50, y: 750, rotation: 60 })
+    .to(".rocket", { duration: 0.05, y: 950, rotation: -55 })
+    .to(".rocket", { duration: 0.05, x: 260, y: 1100, rotation: -55 })
+    .to(".rocket", { duration: 0.2, x: 375, y: 1300, rotation: 0 })
+    .to(".rocket", { duration: 0.2, y: 1600, rotation: 0 });
+});
+
+// Create ScrollTrigger for reversing the animation based on screen size
+ScrollTrigger.create({
+  trigger: ".join-us-section",
+  start: "top top",
+  onEnterBack: () => {
+    // Reverse rocket timeline on scrolling back up
+    rocketTimeline.reverse();
+  },
+  onLeave: () => {
+    // Reset the timeline if scrolling past the join-us-section
+    rocketTimeline.play();
+  },
+});
